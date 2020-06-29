cd35e15ab9e0b689926f58a94c5184ab01a9a743
FarrukhKhan7 <58085304+FarrukhKhan7@users.noreply.github.com>
2020-06-29 13:53:44 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+
+function numberview(num){
+   var result = document.getElementById("result");
+   result.value += num;
+}
+function clearC(){
+    var result = document.getElementById("result");
+    result.value=""
+ }
+ 
+function getresult(){
+    var result = document.getElementById("result");
+    result.value= eval(result.value)
+}
